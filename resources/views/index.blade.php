bfa47076cbce2fc350a79a86c1f101bac2110bd1
raluido <raluido@gmail.com>
2023-11-06 19:37:43 +0000

    i cant use the await without a async funcion

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,45 +16,38 @@
 @section('main')
 
 <script class="" type="module">
-    let find = document.getElementById('inputSearch').value;
-    const options = {
-        method: 'GET',
-        url: 'https://deezerdevs-deezer.p.rapidapi.com/search',
-        params: {
-            q: find
-        },
-        headers: {
-            'X-RapidAPI-Key': 'a0a7b67c0emsh048c4d81f1755d3p1b166cjsnae42641404ab',
-            'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-        }
-    };
+    let find = document.getElementById('inputSearch');
+    find.addEventListener("keypress", function(event) {
+        if (event.key == "Enter") {
+            const options = {
+                method: 'GET',
+                url: 'https://deezerdevs-deezer.p.rapidapi.com/search',
+                params: {
+                    q: find.value
+                },
+                headers: {
+                    'X-RapidAPI-Key': 'a0a7b67c0emsh048c4d81f1755d3p1b166cjsnae42641404ab',
+                    'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+                }
+            };
 
-    try {
-        const response = await axios.request(options);
-        let results = document.getElementById('results');
-        console.log(response.data);
-        if(response.data){
-            (response.data).forEach(element => {
-                let search = document.createElement('li');
-                search.innerHTML = element.title
-                search.appendChild(results);
-            });
+            try {
+                const getData = async function() {
+                    const response = await axios.request(options);
+                    let results = document.getElementById('results');
+                    console.log(response.data.data);
+                    if (response.data.data) {
+                        for (let index = 0; index < response.data.data.length; index++) {
+                            let search = document.createElement('li');
+                            search.innerHTML = response.data.data[index].title;
+                            results.appendChild(search);
+                        };
+                    }
+                }
+            } catch (error) {
+                console.error(error);
+            }
         }
-    } catch (error) {
-        console.error(error);
-    }
+    });
 </script>
 @endsection
-
-
-
-
-
-
-<!-- // const data = axios.get('https://jsonplaceholder.typicode.com/posts')
-// .then(function(respuesta) {
-// console.log(respuesta)
-// })
-// .catch(function(error) {
-// console.log(error)
-// }) -->
